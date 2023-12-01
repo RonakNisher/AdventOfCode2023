049656923fde7a7b28a945047bfb64ec5474467e
Ronak Nisher <nisher.ronak@gmail.com>
2023-12-01 10:03:11 +0530

    initial scaffolding

---
+++ src/day1.rs
@@ -0,0 +1,13 @@
+// #[warn(dead_code)]
+// const NUM: i32 = 2020;
+
+pub fn solve(input: String) {
+
+    input.lines().for_each(|line| {
+		println!("{}", line);
+	});
+
+    println!("*******************");
+    println!("Solved Day 1");
+    println!("*******************");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fs;
+use std::env;
+
+mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let day = &args[1];
+
+    println!("Day: {}", day);
+
+	let filepath = format!("/Users/ronaknisher/advent_of_code_2023/src/day{}_input.txt", day);
+
+	println!("Filepath: {}", filepath);
+    let contents = fs::read_to_string(filepath)
+        .expect("Should have been able to read the file");
+
+    // println!("With text:\n{contents}");
+
+	// contents.lines().for_each(|line| {
+	// 	println!("{}", line);
+	// });
+
+	match day.as_str() {
+		"1" => day1::solve(contents),
+		_ => println!("No solution for day {}", day),
+	}
+
+	// day1::solve();
 }
